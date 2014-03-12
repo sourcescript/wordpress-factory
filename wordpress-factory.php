80b132600084e3d949122f385f9316ec0ca9527a
Gian Santillan <giansantillan18@gmail.com>
2014-03-12 23:45:50 +0800

    fixed plugin

--- wordpress-factory.php
+++ wordpress-factory.php
@@ -1,11 +1,7 @@
 <?php
 	/*
 	Plugin Name: Wordpress Factory
-<<<<<<< HEAD
 	Plugin URI: sourcescript.github.io/wordpress-factory/
-=======
-	Plugin URI: lightshire.github.io/wordpress-factory/
->>>>>>> 6bd519bbd7b61f5d2c6cc7de0c7dbfcf02cb24bf
 	Description: Wordpress Factory Created for Wordpress & Symphony Users
 	Author: Sourcescript
 	Author URI: http://sourcescript.ph
