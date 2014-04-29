81ad589e4a4852469ef0d0cf3490f29cb4d278b4
gian santillan <giansantillan18@gmail.com>
2014-04-30 00:15:18 +0800

    added mail autoload

--- app/app.php
+++ app/app.php
@@ -7,16 +7,18 @@
 					'Core' 		=> base_path().'/../vendor/sourcescript/wf-core-framework/src',
 					'View' 		=> base_path().'/../vendor/sourcescript/wf-core-framework/src',
 					'Assets' 	=> base_path().'/../vendor/sourcescript/wf-core-framework/src',
-					'Twig'		=> base_path().'/../vendor/twig/twig/lib'
+					"Mail" 		=> base_path()."/../vendor/sourcescript/wf-core-framework/src",
+					'Twig'		=> base_path().'/../vendor/twig/twig/lib',
+					'Symfony' 	=> base_path()."/../vendor/symfony/symfony/src"
 				),
 			/*
 			 * Since every plugin requires an options page, the title shall be added here
 			 */
 			'options' 	=> array(
-					'page_title' 	=> 'Wordpress Factory',
-					'menu_title' 	=> 'Wordpress Factory Settings',
+					'page_title' 	=> 'Members Settings',
+					'menu_title' 	=> 'Members Settings',
 					'capability' 	=> 'manage_options',
-					'menu_slug' 	=> 'manage_options'
+					'menu_slug' 	=> 'cupas-members-settings'
 				),
 
 			/*
