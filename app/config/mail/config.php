b0ac9436556de1a0f32405d416b974db1b45e727
gian santillan <giansantillan18@gmail.com>
2014-04-30 00:14:07 +0800

    added config file for mail sending

---
+++ app/config/mail/config.php
@@ -0,0 +1,12 @@
+<?php
+	return array(
+			'is_smtp' 		=> true,
+			'host' 			=> 'smtp.gmail.com',
+			'smtp_auth' 	=> true,
+			'port' 			=> 465,
+			'username' 		=> 'giansantillan18@gmail.com',
+			'password' 		=> 'Qweasd123:',
+			'smtp_secure'	=> 'ssl',
+			'from' 			=> 'giansantillan18@gmail.com',
+			'from_name' 	=> 'Gian Santillan'
+		);
