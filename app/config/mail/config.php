821e680087bf07a2b368d54143c574fc56d938a1
rupaheizu <roop@hakz.co>
2014-05-29 15:47:40 +0800

    Security issue #1

--- app/config/mail/config.php
+++ app/config/mail/config.php
@@ -1,12 +1,13 @@
 <?php
-	return array(
-			'is_smtp' 		=> true,
-			'host' 			=> 'smtp.gmail.com',
-			'smtp_auth' 	=> true,
-			'port' 			=> 465,
-			'username' 		=> 'giansantillan18@gmail.com',
-			'password' 		=> 'Qweasd123:',
-			'smtp_secure'	=> 'ssl',
-			'from' 			=> 'giansantillan18@gmail.com',
-			'from_name' 	=> 'Gian Santillan'
-		);
+
+return array(
+    'is_smtp'       => true,
+    'host'          => '',
+    'smtp_auth'     => true,
+    'port'          => 465,
+    'username'      => '',
+    'password'      => '',
+    'smtp_secure'   => '',
+    'from'          => '',
+    'from_name'     => ''
+);
